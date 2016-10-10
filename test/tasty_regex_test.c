33d2222799869d597fda0df0a7c3819de347ea71
Reid Paape <wrpaape@gmail.com>
2016-10-10 16:55:08 -0500

    wew nm 2 ez

--- test/tasty_regex_test.c
+++ test/tasty_regex_test.c
@@ -21,12 +21,12 @@ test_tasty_regex(void)
 
 	TEST_ASSERT_EQUAL_INT(0,
 			      tasty_regex_compile(&regex,
-						  "a*a*a*a*b"));
+						  "(ba?a?a?b)*ao"));
 
 	TEST_ASSERT_EQUAL_INT(0,
 			      tasty_regex_run(&regex,
 					      &matches,
-					      "baababa"));
+					      "baabbabaooobbbabb"));
 
 	for (struct TastyMatch *restrict match = matches.from;
 	     match < matches.until;
